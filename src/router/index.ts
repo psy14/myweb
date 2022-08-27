d23f6bf01ebff1885042a29ccd35ce243d730ad7
jack <xx@xx.com>
2022-08-27 16:59:15 +0800

    index

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/",
+      redirect: '/airecognize'
+    },
+    {
       path: "/airecognize",
       name: "AI識別",
       component: AiRecognize,
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,15 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig( ({ command, mode }) => {
+  const publicpath = command === 'serve'? './': '/myweb'
+  return {
+    base: publicpath,
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+  }
 });
