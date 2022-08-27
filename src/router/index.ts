90a0f5d6a7b53f792733788a26de58998b84ac0d
jack <xx@xx.com>
2022-08-27 16:16:24 +0800

    router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, type RouterOptions } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AiRecognize from "../views/AiRecognize.vue";
 
