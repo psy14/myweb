1cc5fb352142061a5a76d0cbdacdb04c540566d3
jack <xx@xx.com>
2022-08-31 14:35:42 +0800

    add gif

--- src/views/AiRecognize.vue
+++ src/views/AiRecognize.vue
@@ -1,33 +1,36 @@
 <script setup lang="ts">
 import Navigation from "../components/Navigation.vue";
+import play2 from '@/assets/play2.gif'
 </script>
 
 <template>
-<div class="main">
-    <div class="left">
-        <div class="video"></div>
-    </div>
-    <div class="right">
-        <div class="memo">
-            <div class="memo-block tech">
-                <div>技術實現</div>
-                <div>1. 采用AI技術最新研究成果結合公司自主研發力量實現</div>
-                <div>2. 達到毫秒級識別速度</div>
-                <div>3. 純軟件實現，不需要采購硬件設備</div>
+    <div class="main">
+        <div class="left">
+            <div class="video">
+                <el-image :src="play2" />
             </div>
-            <div class="memo-block benifits">
-                <div>優勢</div>
-                <div>1. 加快游戲速度。根據統計，單桌每日游戲次數提高30%</div>
-                <div>2. 減少人工錯誤。有效減少人工輸入錯誤和識別錯誤</div>
-                <div>3. 提高自動化水平，減少人工支出</div>
+        </div>
+        <div class="right">
+            <div class="memo">
+                <div class="memo-block tech">
+                    <div>技術實現</div>
+                    <div>1. 采用AI技術最新研究成果結合公司自主研發力量實現</div>
+                    <div>2. 達到毫秒級識別速度</div>
+                    <div>3. 純軟件實現，不需要采購硬件設備</div>
+                </div>
+                <div class="memo-block benifits">
+                    <div>優勢</div>
+                    <div>1. 加快游戲速度。根據統計，單桌每日游戲次數提高30%</div>
+                    <div>2. 減少人工錯誤。有效減少人工輸入錯誤和識別錯誤</div>
+                    <div>3. 提高自動化水平，減少人工支出</div>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </template>
 
 <style scoped>
-.main{
+.main {
     display: flex;
     width: 90%;
     margin: 50px auto;
@@ -35,16 +38,27 @@ import Navigation from "../components/Navigation.vue";
     flex-flow: row wrap;
 }
 
+@media (max-width: 600px) {
+    .main {
+        display: flex;
+        width: 90%;
+        margin: 50px auto;
+        justify-content: space-between;
+        flex-flow: column nowrap;
+    }
+}
+
+
 .left {
     flex-basis: 40%;
 }
+
 .right {
     flex-basis: 50%;
 }
 
 .video {
-    width:  570px;
-    height: 393px;
+    width: 100%;
     background: gray;
 }
 
@@ -52,6 +66,7 @@ import Navigation from "../components/Navigation.vue";
     display: flex;
     flex-flow: column;
 }
+
 .memo-block {
     display: flex;
     flex-flow: column;
@@ -61,6 +76,6 @@ import Navigation from "../components/Navigation.vue";
 }
 
 .benifits {
-  margin-top: 30px;  
+    margin-top: 30px;
 }
 </style>
